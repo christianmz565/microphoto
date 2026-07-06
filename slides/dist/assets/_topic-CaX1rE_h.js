f41374c7f2feefcf18bafada7075fc1b391c9692
gustadev121 <lgsc21211@gmail.com>
2026-07-05 23:28:54 -0500

    chore: remove dist from sldies dir

--- slides/dist/assets/_topic-CaX1rE_h.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-2cKFcbga.js";const t=({error:r})=>o.jsx("div",{children:r.message});export{t as errorComponent};
--- slides/dist/assets/route-CIkKhTRE.js
+++
@@ -1,1 +0,0 @@
-import{j as e,O as t}from"./index-2cKFcbga.js";function s(){return e.jsx("div",{className:"w-screen h-screen relative ",children:e.jsx(t,{})})}export{s as component};
